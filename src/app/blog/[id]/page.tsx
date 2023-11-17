a8e687dd983620eb4b2b9bff8dc7491e68f644f8
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-11-17 23:44:02 +0200

    Update blog and blog id pages

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,8 +1,37 @@
+import styles from './page.module.scss';
+import Image from "next/image";
+
 const BlogId = () => {
     return (
-        <div>
-            BlogId
-        </div>
+        <section className="blog__container">
+            <div className={styles.top}>
+                <div className={styles.info}>
+                    <h1 className={styles.title}>Title</h1>
+                    <p className={styles.desc}>Desc</p>
+                    <div className={styles.author}>
+                        <Image
+                            src=""
+                            alt=""
+                            width={40}
+                            height={40}
+                            className={styles.avatar}
+                        />
+                        <span className={styles.username}>username</span>
+                    </div>
+                </div>
+                <div className={styles.imageCont}>
+                    <Image
+                        src=""
+                        alt=""
+                        fill={true}
+                        className={styles.image}
+                    />
+                </div>
+            </div>
+            <div className={styles.content}>
+                <p className={styles.text}>desc</p>
+            </div>
+        </section>
     )
 }
 
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,41 @@
 import styles from './page.module.scss';
+import Link from "next/link";
+import Image from "next/image";
 
 
 export default function Blog() {
     return (
         <section className="blog__container">
-            Blog
+            <Link href='blog/test' className={styles.cont} key={1}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src='https://img.freepik.com/free-vector/illustration-of-gallery-icon_53876-27002.jpg?w=826&t=st=1700256486~exp=1700257086~hmac=d739b1599c4eb6ef0a5e685002d35539bf16bcb929b9e0ab617c0a9dd008841a'
+                        alt='img'
+                        width={400}
+                        height={250}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h2 className={styles.title}>Title</h2>
+                    <p className={styles.desc}>Desc</p>
+                </div>
+            </Link>
+            <Link href='blog/test2' className={styles.cont} key={1}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src='https://img.freepik.com/free-vector/illustration-of-gallery-icon_53876-27002.jpg?w=826&t=st=1700256486~exp=1700257086~hmac=d739b1599c4eb6ef0a5e685002d35539bf16bcb929b9e0ab617c0a9dd008841a'
+                        alt='img'
+                        width={400}
+                        height={250}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h2 className={styles.title}>Title</h2>
+                    <p className={styles.desc}>Desc</p>
+                </div>
+            </Link>
         </section>
     )
 }
