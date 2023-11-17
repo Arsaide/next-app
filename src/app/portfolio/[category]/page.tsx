5d2e8010519389015ef5618eb4781c27118d5017
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-11-17 23:23:32 +0200

    Update

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -22,6 +22,21 @@ const Category = ({params}: any) => {
                         />
                     </div>
                 </div>
+                <div className={styles.item} key={2}>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>test</h1>
+                        <p className={styles.desc}>desc</p>
+                        <Button text="See More" url="#" />
+                    </div>
+                    <div className={styles.imgContent}>
+                        <Image
+                            src="https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b"
+                            className={styles.img}
+                            fill={true}
+                            alt=""
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     )
