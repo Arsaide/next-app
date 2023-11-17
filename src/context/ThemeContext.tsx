024d9cb462e94bf57b0770719a8a09a8810f4987
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-11-18 01:08:35 +0200

    Update the ThemeContext.tsx file. Added saved theme mode to localStorage.

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,6 +1,35 @@
+// "use client";
+//
+// import React, { createContext, useState, ReactNode } from "react";
+//
+// interface ThemeContextProps {
+//     toggle: () => void;
+//     mode: string;
+// }
+//
+// export const ThemeContext = createContext<ThemeContextProps>({
+//     toggle: () => {},
+//     mode: "dark",
+// });
+//
+// interface ThemeProviderProps {
+//     children: ReactNode;
+// }
+//
+// export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+//     const [mode, setMode] = useState("dark");
+//     const toggle = () => setMode((prev) => (prev === "dark" ? "light" : "dark"));
+//
+//     return (
+//         <ThemeContext.Provider value={{ toggle, mode }}>
+//             <div className={`theme ${mode}`}>{children}</div>
+//         </ThemeContext.Provider>
+//     );
+// };
+
 "use client";
 
-import React, { createContext, useState, ReactNode } from "react";
+import React, { createContext, useState, ReactNode, useEffect } from "react";
 
 interface ThemeContextProps {
     toggle: () => void;
@@ -17,7 +46,17 @@ interface ThemeProviderProps {
 }
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-    const [mode, setMode] = useState("dark");
+    const [mode, setMode] = useState<string>(() => {
+        // Проверяем, есть ли значение в localStorage, и используем его, если есть
+        const storedMode = localStorage.getItem("themeMode");
+        return storedMode ? storedMode : "dark";
+    });
+
+    useEffect(() => {
+        // При изменении режима, сохраняем его в localStorage
+        localStorage.setItem("themeMode", mode);
+    }, [mode]);
+
     const toggle = () => setMode((prev) => (prev === "dark" ? "light" : "dark"));
 
     return (
